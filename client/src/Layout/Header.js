894800562580caba9fa68344c9dacda06ea7dc0e
Mustafa <80291704+kahramanX@users.noreply.github.com>
2022-05-29 15:40:44 +0300

    dark mode removed

--- client/src/Layout/Header.js
+++ client/src/Layout/Header.js
@@ -1,31 +1,8 @@
-//React
-import { useEffect } from "react";
-
-//Redux
-import { useDispatch, useSelector } from "react-redux";
-import { setThemeMode } from "../features/site";
 function Header() {
-  const { themeMode } = useSelector((state) => state.site);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    document.querySelector("body").dataset.theme = themeMode;
-  }, [themeMode]);
-
   return (
     <header>
       <div>NFT Searcher 🛰️</div>
       <div>Hello 👋</div>
-      <input
-        type={"checkbox"}
-        onChange={(e) => {
-          if (e.target.checked) {
-            dispatch(setThemeMode("dark"));
-          } else {
-            dispatch(setThemeMode("light"));
-          }
-        }}
-      />
     </header>
   );
 }
