3504c8e2d0501ae47ddd8034c2878c4ada6b7911
Mustafa <80291704+kahramanX@users.noreply.github.com>
2022-05-31 17:36:44 +0300

    firebase test

--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,28 @@ import App from "./App";
 import Header from "./Layout/Header";
 import Footer from "./Layout/Footer";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDO1wru6GvauF0ZHNafFWke_TV_B6W87Mk",
+  authDomain: "project-1-fb684.firebaseapp.com",
+  projectId: "project-1-fb684",
+  storageBucket: "project-1-fb684.appspot.com",
+  messagingSenderId: "189523530914",
+  appId: "1:189523530914:web:f7895bd2e14abd1ad9b3fb",
+  measurementId: "G-K3GD8VMFM6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById("I-LOVE-NFTS"));
 
 root.render(
